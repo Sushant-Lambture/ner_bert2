8dd3f09b5718b9190cb5482e3be23eff9f92f6f9
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-02-03 16:28:16 +0530

    Update run_ner.py

--- run_ner.py
+++ run_ner.py
@@ -17,11 +17,8 @@ from seqeval.metrics import classification_report
 
 from model import BertNer
 from optimization import AdamWeightDecay, WarmUp
-# from bert import tokenization
-# from bert import bert_tokenization
-# from bert_tokenization import FullTokenizer
-import bert_tokenizer as tokenization
-from tokenization import FullTokenizer
+from bert import bert_tokenization
+from bert_tokenization import FullTokenizer
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
